f567f5bb147b762509744fdb8ef0873c6a1fa4dc
AlexPoddubny <usatij.alex@gmail.com>
2020-02-13 23:43:01 +0200

    Template Integration 3

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -572,14 +572,14 @@
     </div>
 </footer>
 
-<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
-<script type="text/javascript" src="js/jquery.nav.js"></script>
-<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="js/jquery.isotope.js"></script>
-<script type="text/javascript" src="js/wow.js"></script>
-<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="{{ asset('assets/js/jquery-1.11.0.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/jquery-scrolltofixed.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/jquery.nav.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/jquery.easing.1.3.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/jquery.isotope.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/wow.js') }}"></script>
+<script type="text/javascript" src="{{ asset('assets/js/custom.js') }}"></script>
 
 </body>
 </html>
