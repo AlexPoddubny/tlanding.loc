2b4ed7da3f2c4e6562053e2d24f283163bba6bdd
AlexPoddubny <usatij.alex@gmail.com>
2020-02-14 12:36:59 +0200

    Template integration

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,13 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Page;
+use App\People;
+use App\Portfolio;
+use App\Service;
 
 class IndexController extends Controller
 {
     //
 	public function execute(Request $request)
 	{
-		return view('layouts.site');
+		$pages = Page::all();
+		$peoples = People::all();
+		$portfolios = Portfolio::take(3)->get();
+		$services = Service::all();
+//		dump($peoples);
+		return view('site.index');
 	}
 	
 }
