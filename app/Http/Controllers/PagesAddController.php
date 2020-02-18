c6b7bf645cfae48b658ef7e1de32a3e2ff22a7b1
AlexPoddubny <usatij.alex@gmail.com>
2020-02-18 16:40:34 +0200

    PagesAddController

--- app/Http/Controllers/PagesAddController.php
+++ app/Http/Controllers/PagesAddController.php
@@ -5,11 +5,27 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
 
 class PagesAddController extends Controller
 {
 	public function execute(Request $request)
 	{
+		
+		if($request->isMethod('post')){
+			$input = $request->except('_token');
+			
+			$validator = Validator::make($input, [
+				'name' => 'required|max:255',
+				'alias' => 'required|unique:pages|max:255',
+				'text' => 'required'
+			]);
+			
+			if ($validator->fails()){
+				return redirect()->route('pagesAdd')->withErrors($validator)->withInput();
+			}
+		}
+		
 		if(view()->exists('admin.pages_add')){
 			$data = [
 				'title' => 'New Page'
@@ -17,8 +33,5 @@ class PagesAddController extends Controller
 			return view('admin.pages_add', $data);
 		}
 		
-		if($request->isMethod('post')){
-		
-		}
     }
 }
